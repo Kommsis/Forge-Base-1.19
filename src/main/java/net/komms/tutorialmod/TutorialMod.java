27759a088ebbcc733d3ed1625967cddb6b5c2e08
cleoh <crimsonis.proatbeingpro@gmail.com>
2022-08-14 23:30:49 -0400

    Added moditems innit

--- src/main/java/net/komms/tutorialmod/TutorialMod.java
+++ src/main/java/net/komms/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.komms.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.komms.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,6 +20,7 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
